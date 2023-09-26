51c8a86d54a7b67676fad85e962dc31ca586cd06
Priyanka-Nade2000 <nadeps17@gmail.com>
2023-09-26 14:51:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        This My First Netlify Application
         </a>
       </header>
     </div>
